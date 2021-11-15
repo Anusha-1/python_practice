1c6c25dc3f67363ecce3fcaac0786b47be7c7aca
Anusha-1 <32974939+Anusha-1@users.noreply.github.com>
2021-11-15 15:57:09 +0530

    Update parse_csv.py

--- data/parse_csv.py
+++ data/parse_csv.py
@@ -1,22 +1,7 @@
 #csv = json ==dict similar formats
 
 # with open will take care of memory leaks
-'''
-with open('names.csv','r') as csvfile:
-    csv_reader=csv.DictReader(csvfile) #dict
-    
-    with open('new_names.csv','w') as newfile:
-        fnames=['first_name','last_name']
-        csv_writer=csv.DictWriter(newfile,fieldnames=fnames,delimiter=',',lineterminator='\n')
-        csv_writer.writeheader()
-        for line in csv_reader:
-            del line['email']
-            csv_writer.writerow(line)
-        print('File created successfully')
 
-print('csv export completed')
-
-'''
 import csv
 with open('names.csv','r') as csvfile:
     csv_reader=csv.DictReader(csvfile) #dict
