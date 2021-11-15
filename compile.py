68ab6c9720a1d96f3fd350a80fb748b5342ae78a
Anusha-1 <32974939+Anusha-1@users.noreply.github.com>
2021-11-15 15:24:30 +0530

    Create compile.py

---
+++ compile.py
@@ -0,0 +1,6 @@
+#we say python is interpretor language. But there is inbuilt compiler in python which does the compilation too.
+import py_compile
+py_compile.compile('app.py')
+print('COMPILATION DONE')
+
+#this produces the compilation file in cache.
