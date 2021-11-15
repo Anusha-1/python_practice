b38101ed21ab337373b3aee940c20cfb1ea8601a
Anusha-1 <32974939+Anusha-1@users.noreply.github.com>
2021-11-15 16:11:56 +0530

    Create pool_processing.py

---
+++ scalability/pool_processing.py
@@ -0,0 +1,23 @@
+# Python program to understand 
+# the concept of pool 
+import multiprocessing 
+import os 
+
+def square(n): 
+	print("Worker process id for {0}: {1}".format(n, os.getpid())) 
+	return (n*n) 
+
+if __name__ == "__main__": 
+	# input list 
+	mylist = [1,2,3,4,5] 
+
+	# creating a pool object 
+	p = multiprocessing.Pool() 
+
+	# map list to target function 
+	result = p.map(square, mylist) 
+
+	print(result) 
+
+
+#so we can apply this kind of concept to 1 million and see the tremendous performance improvement 
